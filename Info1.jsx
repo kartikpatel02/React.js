af0a6a9e062f8995366fb37588566dcc6cba8466
kartikpatel02 <84899372+kartikpatel02@users.noreply.github.com>
2021-06-26 19:36:36 +0530

    Add files via upload

---
+++ Info1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info1() {
+  return (
+    <div className="note">
+      <h1>Kartik Patel 1</h1>
+      <p>My hobbies are book reading</p>
+    </div>
+  );
+}
+
+export default Info1;
---
+++ Info3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info3() {
+  return (
+    <div className="note">
+      <h1>Kartik Patel 3</h1>
+      <p>Data about me</p>
+    </div>
+  );
+}
+
+export default Info3;
